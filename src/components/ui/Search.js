cc0798b8ec53fbb35251345e7f58edd4edf539f4
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2022-03-25 20:20:08 +0700

    Remove unused import

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Search = ({ query, setQuery }) => {
-  // const [text, setText] = useState("");
   return (
     <section className="search">
       <form>
