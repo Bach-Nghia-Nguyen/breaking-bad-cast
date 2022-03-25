72d79f75cad7fdf269c0b44bccc319a31558df83
Bach Nghia Nguyen <bachnghianguyen@gmail.com>
2022-03-25 16:28:39 +0700

    Get all characters API and render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
 import "./App.css";
+import axios from "axios";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log("BB api result", result);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
 };
